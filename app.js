b6fe3252b56401213ce1cae570db245dbdaf4159
gondolio <oren.sivan@gmail.com>
2019-06-08 10:37:47 +0900

    HTTPS

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ connect.then((db) => {
 
 const app = express();
 
+// Redirect all HTTP requests to HTTPS
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    res.redirect(`https://${req.hostname}:${app.get('secPort')}${req.url}`)
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
